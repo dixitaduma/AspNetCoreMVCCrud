a831fd49dd4a4431d69beaa8d96792cc00a40bab
dixitaduma <dixitaduma@gmail.com>
2021-08-10 10:36:30 +0530

    CRUD Operation

---
+++ CoreMVCNew_09-08/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using CoreMVCNew_09_08.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCNew_09_08.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly StdContext _context;
+
+        public HomeController(StdContext context,ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.Students.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Students.Add(std);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(std);
+        }
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+            Student std = _context.Students.Find(id);
+            if (std == null)
+            {
+                return View("Index");
+            }
+            return View(std);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+            Student std = _context.Students.Find(id);
+            if (std == null)
+            {
+                return View("Index");
+            }
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            if (id != 0)
+            {
+                Student std = _context.Students.Find(id);
+                _context.Students.Remove(std);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+            Student std = _context.Students.Find(id);
+            if (std == null)
+            {
+                return View("Index");
+            }
+            return View(std);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Entry(std).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(std);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CoreMVCNew_09-08/Models/Std.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCNew_09_08.Models
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key()]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Please enter correct email")]
+        public string Email { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "age must be between 1 and 100")]
+        public int Age { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+    }
+
+    public class StdContext : DbContext
+    {
+        public StdContext(DbContextOptions<StdContext> options) : base(options)
+        {
+        }
+        public virtual DbSet<Student> Students { get; set; }
+    }
+
+}
